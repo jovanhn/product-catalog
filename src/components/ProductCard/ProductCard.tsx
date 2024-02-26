1b92ef5858951035b644d3707e056ebcb94e018a
Jovan Suberic <jovan.suberic@fioneer.com>
2024-02-26 20:13:54 +0100

    switch product grid item to the product card item

---
+++ src/components/ProductCard/ProductCard.tsx
@@ -0,0 +1,38 @@
+
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+import {Product} from "../../interfaces/entities.tsx";
+import {useNavigate} from "react-router-dom";
+
+
+export default function ProductCard({product}:{product: Product}) {
+
+    const navigate = useNavigate();
+    function handleClick(productId: number) {
+        navigate(`/products/${productId}`)
+    }
+    return (
+        <Card sx={{ maxWidth: 345 }}>
+            <CardActionArea onClick={()=> handleClick(product.id)}>
+                <CardMedia
+                    component="img"
+                    height="140"
+                    image={product.images[0]}
+                    alt={product.title}
+                />
+
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                        {product.title}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        {product.description}
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
+        </Card>
+    );
+}
--- src/components/ProductsViews/AllProductsGrid.tsx
+++ src/components/ProductsViews/AllProductsGrid.tsx
@@ -1,17 +1,19 @@
 import Grid from '@mui/material/Unstable_Grid2';
-import ComplexGrid from '../ComplexGrid/ComplexGrid.tsx';
+// import ComplexGrid from '../ComplexGrid/ComplexGrid.tsx';
 
 import { Product } from '../../interfaces/entities.tsx';
+import ProductCard from "../ProductCard/ProductCard.tsx";
 
 function AllProductsGrid({products}: {products: Product[]}) {
 
     return (
         <>
-            <Grid container rowSpacing={3} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+            <Grid container rowSpacing={4} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
 
                 {products.map((product) =>
-                    <Grid xs={6} key={product.id}>
-                        <ComplexGrid product={product} />
+                    <Grid xs={4} key={product.id}>
+                        <ProductCard product={product}/>
+                        {/*<ComplexGrid product={product} />*/}
                     </Grid>)}
             </Grid>
         </>
